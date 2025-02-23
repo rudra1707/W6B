fa8815970f32d6ddebf3ba80a6ec6190f05be1af
rudra <bhattrudra17072005@gmail.com>
2025-02-23 16:35:00 -0500

    Configuration loading, log transmission, and local file logging features were added to TestClient.java's basic structure

---
+++ TestClient.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class TestClient {
+    private static String serverIP;
+    private static int serverPort;
+    private static final String LOG_FILE = "client_logs.txt";
